19581034bd0e961de760345329b4084ff8a6c4c8
surajbarailee <baraileesuraj@gmail.com>
2022-02-15 21:35:52 +0545

    trying requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,14 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=bf9f22e5d8fa480f75fdadafc480b783&query=27.71,85.32";
+  "http://api.weatherstack.com/current?access_key=bf9f22e5d8fa480f75fdadafc480b783&query=27.71,85.32&units=m";
 
-request({ url: url }, (error,response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+  current = response.body.current;
+  console.log(
+    "It is currently " +
+      current.temperature +
+      " and feels like " +
+      current.feelslike
+  );
 });
