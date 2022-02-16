2864b9804fb213a79ea65af789ec625578e04546
surajbarailee <baraileesuraj@gmail.com>
2022-02-16 21:19:59 +0545

    9/19

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,16 @@
 const request = require("request");
+const geoCode = require('./utils/geo_code')
 
-const url =
-  "http://api.weatherstack.com/current?access_key=bf9f22e5d8fa480f75fdadafc480b783&query=27.71,85.32&units=m";
 
-request({ url: url, json: true }, (error, response) => {
-  current = response.body.current;
-  console.log(
-    "It is currently " +
-      current.temperature +
-      " and feels like " +
-      current.feelslike
-  );
+
+
+
+geoCode("Philadelphia New York", (error, data) => {
+  if (error){
+    console.log(error)
+  }
+  else{
+
+    console.log(data);
+  }
 });
---
+++ weather-app/utils/geo_code.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const geoCode = (address, callback) => {
+    const encoded_address = encodeURIComponent(address);
+  
+    const url =
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+      encoded_address +
+      ".json?access_token=pk.eyJ1Ijoic3VyYWpiYXJhaWxlZSIsImEiOiJja3pwcDFnMDkwbmhxMnBwZTl4ZnNkc2RiIn0.94MNEdjLDaObSBL6kjczQA";
+    
+    request({ url: url, json: true }, (error, response) => {
+      if (error) {
+        callback("Unable to Connect to location service", undefined);
+      } else if (response.body.features.length === 0) {
+        callback("Unable to Connect to location service", undefined);
+      } else {
+        callback(undefined, {
+          latitude: response.body.features[0].center[0],
+          longitude: response.body.features[0].center[1],
+          location: response.body.features[0].place_name,
+        });
+      }
+    });
+  };
+
+module.exports = geoCode
